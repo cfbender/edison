091181d8804c6e2965f852d648f9fe807f63b853
Cody Bender <cody.bender@pdq.com>
2021-04-01 13:36:42 -0600

    fix: put covid config in prod

--- config/prod.exs
+++ config/prod.exs
@@ -7,4 +7,5 @@ config :nostrum,
 config :edison,
   mechmarket_role_id: System.get_env("EDISON_MECHMARKET_ROLE_ID") |> String.to_integer(),
   mechmarket_channel: System.get_env("EDISON_MECHMARKET_CHANNEL") |> String.to_integer(),
-  mechmarket_query: System.get_env("EDISON_MECHMARKET_QUERY")
+  mechmarket_query: System.get_env("EDISON_MECHMARKET_QUERY"),
+  covid_channel: System.get_env("EDISON_COVID_CHANNEL") |> String.to_integer()
