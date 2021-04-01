3c3438ee6ee37546138772affdbe05bbf15ea037
Cody Bender <cody.bender@pdq.com>
2021-04-01 13:32:01 -0600

    feat: vaccine appt scraper

--- config/dev.exs
+++ config/dev.exs
@@ -1,3 +1,9 @@
 import Config
 
 import_config "dev.secret.exs"
+
+config :edison,
+  mechmarket_role_id: System.get_env("EDISON_MECHMARKET_ROLE_ID") |> String.to_integer(),
+  mechmarket_channel: System.get_env("EDISON_MECHMARKET_CHANNEL") |> String.to_integer(),
+  mechmarket_query: System.get_env("EDISON_MECHMARKET_QUERY"),
+  covid_channel: System.get_env("EDISON_COVID_CHANNEL") |> String.to_integer()
